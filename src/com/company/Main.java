c55da93ff71f5d4177933f0330f93237f189a60e
Conner Caspar <conner.caspar@gmail.com>
2018-01-31 16:31:16 -0500

    OOP notes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Cat cat = new Cat(4, true, "Calico");
 
+        System.out.println(cat.getColor());
     }
 }
